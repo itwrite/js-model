9282dda0deb95a22ae1e2a22fe4315388ffb49f2
itwrite <itwrite@163.com>
2017-12-18 19:58:05 +0800

    Update model-last.js

--- model-last.js
+++ model-last.js
@@ -4,12 +4,6 @@
 
 (function (global, factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
-        // For CommonJS and CommonJS-like environments where a proper `window`
-        // is present, execute the factory and get Model.
-        // For environments that do not have a `window` with a `document`
-        // (such as Node.js), expose a factory as module.exports.
-        // This accentuates the need for the creation of a real `window`.
-        // e.g. var Model = require("model")(window);
         module.exports = global.document ?
             factory(global, true) :
             function (w) {
